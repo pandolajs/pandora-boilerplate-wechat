3a5b44a4845b36c2596ca09e20adf9580bc3e1e1
houquan <houquan@babytree-inc.com>
2018-06-25 21:30:19 +0800

    feat: 项目模板初始化；

---
+++ src/components/demo/index.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview Demo 组件脚本
+ * @author sizhao | 870301137@qq.com
+ * @version 1.0.0 | 2018-06-25 | sizhao
+*/
+
+Component({
+  properties: {
+    name: String
+  }
+})
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * @fileOverview 工具方法
+ * @author sizhao | 870301137@qq.com
+ * @version 1.0.0 | 2018-06-25 | sizhao
+*/
+
+export const sayHello = () => {
+  console.log('Hello, Pandolajs!')
+}
